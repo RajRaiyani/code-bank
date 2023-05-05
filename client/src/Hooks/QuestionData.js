1bbaa9a32949734ef5611270eebc66fe125ebe40
harshilvasoya <harshilvasoya2812@gmail.com>
2023-05-06 00:06:15 +0530

    create add question page

---
+++ client/src/Hooks/QuestionData.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import cookie from "js-cookie";
+
+var QuestionDatach = () => {
+	
+	const [getdata, setGetdata] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3007/api/v1/question", {
+			method: "GET",
+			headers: {
+				'Content-Type': 'application/json',
+				"token": cookie.get("token")
+			}
+		}).then(res => res.json())
+			.then(res => {
+				console.log(res.data)
+				if (res.status === "ok") {
+					setGetdata(res.data);
+				}
+			})
+			.catch(e => console.log("error : " + e));
+	}, []);
+	return [getdata , setGetdata];
+}
+
+export default QuestionDatach;
