65ddf96326451a05b0fcb6d5a2f2eec4f63211f0
RajRaiyani <rpraiyani7@gmail.com>
2023-05-05 22:20:35 +0530

    changed .env and added new connection string

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,7 +4,7 @@ const router  = express.Router();
 const isAdminLoggedIn = require("./../middlewares/isAdminLoggedIn");
 const {addQuestion, deleteQuestion}  = require("./../controllers/admin/questionController");
 
-router.route("/question/add").post(isAdminLoggedIn,addQuestion)
+router.route("/question/add").post(isAdminLoggedIn,addQuestion);
 router.route("/question/delete").delete(isAdminLoggedIn,deleteQuestion);
 
 module.exports = router;
