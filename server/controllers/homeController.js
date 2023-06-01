bb4b13a01feafb3a69ec594c22d6226cdf87cf55
RajRaiyani <rpraiyani7@gmail.com>
2023-06-02 00:54:18 +0530

    added like route

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -3,6 +3,7 @@ const Question = require("./../models/question");
 const Solution = require("./../models/solution");
 const User = require("./../models/user");
 const List = require("./../models/list");
+const Like = require("./../models/like");
 
 
 
@@ -24,14 +25,51 @@ exports.getAllQuestions = async (req,res) => {
 exports.getOneQuestion = async (req,res) => {
 	try{
 		var data = await Question.findOne({_id:req.params.id},{});
-		var solutions = await Solution.find({question_id:req.params.id},{question_id:0});
+
 		if(!data) return res.json({status:"NOT_EXIST",message:"question does not exist."});
+
+		var solutions = await Solution.find({question_id:req.params.id},{question_id:0});
+		
 	}catch(error){
 		return res.json({status:"X",message:"something went wrong.",error})
 	}
 	
 	res.json({status:"OK",data:{...data._doc,solutions}});
 }
+
+exports.likeQuestion = async (req,res) => {
+	var question_id = req.params.id;
+	var user_id = req.user_id;
+	if(!(question_id && user_id)){
+		return res.json({status:"MISSING_FIELD",message:"either question Id or user Id is missing."});
+	}
+	var like = false;
+	try{
+		if(!(await Like.findOneAndRemove({user_id,question_id}))){
+
+			if(!(await Question.updateOne({_id:question_id},{$inc:{likes:1}}))){
+				return res.json({status:"NOT_EXIST",message:"No such question Exist."});
+			}
+			await Like.create({user_id,question_id});
+			
+			like = true;
+		}else{
+			if(!(await Question.updateOne({_id:question_id},{$inc:{likes:-1}}))){
+				return res.json({status:"NOT_EXIST",message:"No such question Exist."});
+			}
+		}
+	}catch(error){
+		return res.json({status:"X",message:"something went wrong while adding like to question.",error})
+	}
+	res.json({status:"OK",like});
+	
+}
+
+
+
+
+
+
 
 //==================== All Users =================================
 
