05267a085986900dd7138dd5205ff0a214388978
utsav <utsavbusa222@gmail.com>
2023-06-09 23:12:53 +0530

    add new admin pannel

--- client/src/pages/admin/AdminApp.js
+++ client/src/pages/admin/AdminApp.js
@@ -1,29 +1,61 @@
-import Footer from "../../components/Footer";
+import "../../sideNavBar.css";
 import { Link, Outlet, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import cookie from "js-cookie";
-
+import { faList, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AdminApp = (props) => {
-    var navigate = useNavigate();
+  var navigate = useNavigate();
 
+  const [isExpanded, setExpendState] = useState(false);
 
-   
+  const menuItems = [
+    {
+      text: "Programs",
+      icon: "images/grid.svg",
+      path: "/admin/Program",
+    },
+    {
+      text: "Admin Profile",
+      icon: "images/user.svg",
+    },
+    {
+      text: "Messages",
+      icon: "images/message.svg",
+    },
+    {
+      text: "Analytics",
+      icon: "images/pie-chart.svg",
+    },
+    {
+      text: "File Manager",
+      icon: "images/folder.svg",
+    },
+    {
+      text: "Orders",
+      icon: "images/shopping-cart.svg",
+    },
+    {
+      text: "Settings",
+      icon: "images/settings.svg",
+    },
+  ];
 
-    useEffect(() => {
-        if (!cookie.get("adminToken")) {
-            navigate("/login");
-        }
-    }, [navigate]);
-
-    function logOut() {
-        cookie.remove("adminToken");
-        navigate("/");
+  useEffect(() => {
+    if (!cookie.get("adminToken")) {
+      navigate("/login");
     }
+  }, [navigate]);
 
-    return (
-        <>
-            <nav className="navbar bg-body-tertiary">
+  function logOut() {
+    cookie.remove("adminToken");
+    navigate("/");
+  }
+
+  return (
+    <>
+      {/* <nav className="navbar bg-body-tertiary">
                 <div className="container-fluid">
                     <button className="btn btn-outline-success px-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span className="fs-5 me-2">&#x205D;</span> {props.name}
@@ -52,11 +84,115 @@ const AdminApp = (props) => {
                 </div>
             </nav>
             <Outlet />
-            <Footer></Footer>
-        </>
-
-    )
-
-
-}
+            <Footer></Footer> */}
+      <div className="d-flex gap-3" style={{ backgroundColor: " #DFE5EF" }}>
+        <div
+          className={
+            isExpanded
+              ? "side-nav-container"
+              : "side-nav-container side-nav-container-NX"
+          }
+        >
+          <div className="nav-upper">
+            <div
+              className={`d-flex ${
+                isExpanded
+                  ? "justify-content-center"
+                  : "justify-content-start p-2"
+              } pt-3`}
+              style={{ paddingLeft: "4px" }}
+            >
+              <div>
+                {isExpanded && (
+                  <div className="nav-brand d-flex">
+                    <img src="images/Logo.svg" alt="" srcset="" />
+                    <h2>Code_Bank </h2>
+                  </div>
+                )}
+              </div>
+              <div
+                className="px-3 d-flex align-items-center justify-content-center"
+                onClick={() => setExpendState(!isExpanded)}
+              >
+                <FontAwesomeIcon
+                  icon={isExpanded === true ? faXmark : faList}
+                  style={{ fontSize: "2rem" }}
+                />
+              </div>
+            </div>
+             
+            <div style={{ marginTop: "30px" }}>
+              {menuItems.map((item, index) => (
+                <Link
+                  to={item.path}
+                  key={index}
+                  className={`text-uppercase text-color-light d-flex ${
+                    isExpanded
+                      ? "justify-content-start pl-2 gap-2"
+                      : "justify-content-center my-3 p-2"
+                  } align-items-center nav-item`}
+                  style={{
+                    fontSize: "1rem",
+                    margin: `${isExpanded ? "4px 20px" : "8px"}`,
+                  }}
+                >
+                  <div>
+                    <img
+                      style={{
+                        margin: `${isExpanded ? "4px 20px" : "8px"}`,
+                      }}
+                      src={item.icon}
+                      fill="black"
+                      width={30}
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <div>
+                    {isExpanded && (
+                      <p style={{ margin: "10px" }}>{item.text}</p>
+                    )}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="nav-footer">
+            <div
+              className={`pi-auto d-flex ${
+                isExpanded ? "justify-content-around" : "justify-content-center"
+              }`}
+            >
+              {isExpanded && (
+                <div className="d-flex">
+                  <img
+                    src="/images/admin-avatar.svg"
+                    alt=""
+                    style={{ width: "100px", padding: "0 20px" }}
+                    srcset=""
+                  />
+                  <div className="nav-footer-info">
+                    <p style={{ fontSize: "18px", fontWeight: "900" }}>hrsu</p>
+                    <p style={{ marginTop: "-15px", color: "gray" }}>
+                      Master admin
+                    </p>
+                  </div>
+                </div>
+              )}
+              <img
+                style={{ margin: `${isExpanded ? "4px 20px" : "8px"}` }}
+                src="images/logout.svg"
+                width={35}
+                alt=""
+                srcset=""
+                onClick={logOut}
+              />
+            </div>
+          </div>
+        </div>
+        <div>{Outlet}</div>
+      </div>
+    </>
+  );
+};
 export default AdminApp;
--- client/src/try.js
+++ client/src/try.js
@@ -1,68 +1,148 @@
+import React, { useState } from "react";
+import "./sideNavBar.css";
+import { faList, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const SideNavBar = () => {
+  const [isExpanded, setExpendState] = useState(false);
 
-var Try = () => {
-
+  const menuItems = [
+    {
+      text: "Dashboard",
+      icon: "images/grid.svg",
+  
+    },
+    {
+      text: "Admin Profile",
+      icon: "images/user.svg",
+    },
+    {
+      text: "Messages",
+      icon: "images/message.svg",
+    },
+    {
+      text: "Analytics",
+      icon: "images/pie-chart.svg",
+    },
+    {
+      text: "File Manager",
+      icon: "images/folder.svg",
+    },
+    {
+      text: "Orders",
+      icon: "images/shopping-cart.svg",
+    },
+    {
+      text: "Settings",
+      icon: "images/settings.svg",
+    },
+  ];
 
   return (
     <>
-  <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{width: "280px",height:"100%"}}>
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      {/* <svg className="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg> */}
-      <span className="fs-4">Sidebar</span>
-    </a>
-    <hr />
-    <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
-          {/* <svg className="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg> */}
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link link-dark">
-          {/* <svg className="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg> */}
-          Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link link-dark">
-          {/* <svg className="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg> */}
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link link-dark">
-          {/* <svg className="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg> */}
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link link-dark">
-          {/* <svg className="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg> */}
-          Customers
-        </a>
-      </li>
-    </ul>
-    <hr />
-    <div className="dropdown">
-      <a href="#" className="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
-        <strong>mdo</strong>
-      </a>
-      <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-        <li><a className="dropdown-item" href="#">New project...</a></li>
-        <li><a className="dropdown-item" href="#">Settings</a></li>
-        <li><a className="dropdown-item" href="#">Profile</a></li>
-        <li><hr className="dropdown-divider" /></li>
-        <li><a className="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
-  </div>
-  
-  
-  
+      <div style={{ backgroundColor: " #DFE5EF" }}>
+        <div
+          className={
+            isExpanded
+              ? "side-nav-container"
+              : "side-nav-container side-nav-container-NX"
+          }
+        >
+          <div className="nav-upper">
+            <div
+              className={`d-flex ${
+                isExpanded
+                  ? "justify-content-center"
+                  : "justify-content-start p-2"
+              } pt-3`}
+              style={{ paddingLeft: "4px" }}
+            >
+              <div>
+                {isExpanded && (
+                  <div className="nav-brand d-flex">
+                    <img src="images/Logo.svg" alt="" srcset="" />
+                    <h2>Code_Bank </h2>
+                  </div>
+                )}
+              </div>
+              <div
+                className="px-3 d-flex align-items-center justify-content-center"
+                onClick={() => setExpendState(!isExpanded)}
+              >
+                <FontAwesomeIcon
+                  icon={isExpanded === true ? faXmark : faList}
+                  style={{ fontSize: "2rem" }}
+                />
+              </div>
+            </div>
+            <div style={{ marginTop: "30px" }}>
+              {menuItems.map(({ text, icon }) => (
+                <a
+                  className={`text-uppercase text-color-light d-flex ${
+                    isExpanded
+                      ? "justify-content-start pl-2 gap-2"
+                      : "justify-content-center my-3 p-2"
+                  } align-items-center nav-item`}
+                  style={{
+                    fontSize: "1rem",
+                    margin: `${isExpanded ? "4px 20px" : "8px"}`,
+                  }}
+                  href="#"
+                >
+                  <div>
+                    <img
+                      style={{ margin: `${isExpanded ? "4px 20px" : "8px"}`,color:"black" }}
+                      src={icon}
+                      fill="black"
+                      width={30}
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <div>
+                    {isExpanded && <p style={{ margin: "10px" }}>{text}</p>}
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="nav-footer">
+            <div
+              className={`pi-auto d-flex ${
+                isExpanded ? "justify-content-around" : "justify-content-center"
+              }`}
+            >
+              {isExpanded && (
+                <div className="d-flex">
+                  <img
+                    src="/images/admin-avatar.svg"
+                    alt=""
+                    style={{ width: "100px", padding: "0 20px" }}
+                    srcset=""
+                  />
+                  <div className="nav-footer-info">
+                    <p style={{ fontSize: "18px", fontWeight: "900" }}>
+                     hrsu
+                    </p>
+                    <p style={{ marginTop: "-15px", color: "gray" }}>
+                    Master admin
+                    </p>
+                  </div>
+                </div>
+              )}
+              <img
+                style={{ margin: `${isExpanded ? "4px 20px" : "8px"}` }}
+                src="images/logout.svg"
+                width={35}
+                alt=""
+                srcset=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Try;
+export default SideNavBar;
