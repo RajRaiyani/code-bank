525015234e1c7f1838bc2980846e8e76c37be088
RajRaiyani <rpraiyani7@gmail.com>
2023-06-11 14:23:15 +0530

    added is liked route

--- server/App.js
+++ server/App.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const cors=require("cors");
+const cors = require("cors");
 require("./config/database").connect();
 const express = require("express");
 
@@ -7,17 +7,17 @@ const express = require("express");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 const authentication = require("./routes/authentication");
 const admin = require("./routes/admin");
 const home = require("./routes/home");
 const test = require("./routes/test");
 
-app.use("/api/v1/home",home);
-app.use("/api/v1/authentication",authentication);
-app.use("/api/v1/admin",admin);
-app.use("/api/v1/test",test);
+app.use("/api/v1/home", home);
+app.use("/api/v1/authentication", authentication);
+app.use("/api/v1/admin", admin);
+app.use("/api/v1/test", test);
 
 module.exports = app;
 
