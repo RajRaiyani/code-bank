0764162a8d24faa14033766e0f2c2a7f038506c8
RajRaiyani <rpraiyani7@gmail.com>
2023-08-13 22:14:22 +0530

    created user card

--- client/src/pages/Exampal.js
+++ client/src/pages/Exampal.js
@@ -1,13 +1,17 @@
 import CommentCard from "../components/Cards/CommentCard";
-
+import UserCard from "../components/Cards/UserCard"
 const Exampal = () =>{
 
 	const d = new Date();
 	return(
+		<>
 		<div className="p-6 w-1/3">
-			<CommentCard userName="rpraiyani" className="m-2" comment="this is for testing akjd lkfjs jdkfe skdfi string font style and the other" date={d.toDateString()}  />
-			<CommentCard admin userName="rpraiyani" className="m-2" comment="this is for testing akjd lkfjs jdkfe skdfi string font style and the other" date={d.toDateString()}  />
+			<CommentCard username="rpraiyani" className="m-2" comment="this is for testing akjd lkfjs jdkfe skdfi string font style and the other" date={d.toDateString()}  />
+			<CommentCard admin username="rpraiyani" className="m-2" comment="this is for testing akjd lkfjs jdkfe skdfi string font style and the other" date={d.toDateString()}  />
 		</div>
+		<UserCard onRoleChange={()=>console.log("change role")} username="rpraiyani" email="rpraiyani70@gmail.com" role="admin" />
+		<UserCard username="rpraiyani" email="rpraiyani70@gmail.com" role="user" />
+		</>
 	);
 }
 
