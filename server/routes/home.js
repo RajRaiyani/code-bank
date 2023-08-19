191940abf2eaf5beb00157c3524312bb0915281a
RajRaiyani <rpraiyani7@gmail.com>
2023-08-19 18:11:23 +0530

    changed in server

--- server/routes/home.js
+++ server/routes/home.js
@@ -3,11 +3,15 @@ const router  = express.Router();
 
 const isLoggedIn = require("./../middlewares/isLoggedIn");
 const {getAllQuestions,getOneQuestion,getAllUsers, getOneList, getAllLists, likeQuestion, commentOnQuestion} = require("./../controllers/homeController");
+const { getSolutionById } = require("../controllers/admin/solutionController");
 
 router.route("/question").get(getAllQuestions);
 router.route("/question/:id").get(isLoggedIn,getOneQuestion);
 router.route("/question/:id/like").put(isLoggedIn,likeQuestion);
 router.route("/question/:id/comment").post(isLoggedIn,commentOnQuestion);
+
+router.route("/solution/:id").get(getSolutionById);
+
 router.route("/user").get(isLoggedIn,getAllUsers);
 
 router.route("/list/get/").get(getAllLists);
